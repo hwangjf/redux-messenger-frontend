3bf81aa6159650a9652fba80456048c07051bbc6
sinyoungnam <sinnam90@gmail.com>
2019-04-23 00:06:36 -0400

    home page completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './navbar/Navbar.js'
 import Home from './home/Home.js'
+import Signup from './signup/Signup.js'
 import '../sass/main.scss'
 import { connect } from 'react-redux'
 
 class App extends Component {
 
   componentDidMount() {
-    
+
   }
 
   render() {
     return (
       <Router>
-          <Navbar />
-            <Route exact path="/" render={props =>
-              <Home />}/>
+        <Navbar />
+
+        <Route exact path="/" component={ Home } />
+        <Route path="/signup" component={ Signup } />
+
       </Router>
     );
   }
 }
 
-function mapStateToProps() {
+// function mapStateToProps() {
+//
+// }
 
-}
-
-export default connect()(App)
+export default connect(null)(App)
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+      <ul className="navbar__ul">
+        <li className="navbar__li">
+          <Link to="/">Logo</Link>
+        </li>
+      </ul>
+      <a className="navbar__li btn btn__medium" href=" ">Login</a>
+    </div>
+  )
+}
+
+export default Navbar
