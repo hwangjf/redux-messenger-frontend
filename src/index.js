cb336cc4c981d6d60b724b1e937c4dbb4210f77a
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2019-10-08 23:00:57 -0400

    ws url trouble shoot

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import configureStore from './store'
 
 const store = configureStore()
 
-const WS = process.env.NODE_ENV === 'production' ? `ws://${process.env.REACT_APP_API_BASE_URL}/cable` : 'ws://localhost:4000/cable'
+const WS = process.env.NODE_ENV === 'production' ? `wss://${process.env.REACT_APP_API_BASE_URL}/cable` : 'ws://localhost:4000/cable'
 
 ReactDOM.render(
   <ActionCableProvider url={WS}>
