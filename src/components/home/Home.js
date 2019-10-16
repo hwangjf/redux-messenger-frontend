26671a655704ecc625482e66e758dc9de61a08e8
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2019-10-15 22:29:03 -0400

    slight home change

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,32 +4,30 @@ import { connect } from 'react-redux'
 import { signup } from '../../actions/user'
 import Signup from './Signup';
 
-const initialState = {
-  username: '',
-  password: ''
-}
 class Home extends React.Component {
 
-  state = initialState
 
-  handleChange = e => this.setState({[e.target.name]: e.target.value})
+  // handleChange = e => this.setState({[e.target.name]: e.target.value})
 
-  handleSubmit = e => {
-    e.preventDefault()
-    this.props.signup(this.state)
-    this.setState(initialState)
-    this.props.history.push('messages')
-  }
+  // handleSubmit = e => {
+  //   e.preventDefault()
+
+  //   this.props.history.push('messages')
+  // }
 
   render() {
     return (
       <div className="home">
         <div className="home__body">
-          <Signup />
+          {
+            !!this.props.user
+            ? null
+            : <Signup />
+          }
         </div>
       </div>
     )
   }
 }
 
-export default connect(null, { signup } )(Home)
+export default connect(state => ({user: state.user}), { signup } )(Home)
